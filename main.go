00e245bf6fa4dd8953b024e7a6577f6cbaf5413f
Victor Campos <tech+victorcampos@elo7.com>
2015-01-30 12:55:50 -0200

    Adds custom usage message to -h flag

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 )
 
 func main() {
+	setCustomUsageMessage()
+
 	configVars := make(commandline.ConfigVarsMap)
 
 	flag.Var(&configVars, "e", "sets configuration variables")
@@ -28,6 +30,33 @@ func main() {
 
 	err = docker.Build(harborConfig.ImageTag)
 	checkError(err)
+}
+
+func setCustomUsageMessage() {
+	// TODO: Alter the help message when not using /bin/bash anymore
+	flag.Usage = func() {
+		helpText := `Usage: harbor [-e KEY=VALUE]
+
+Harbor looks up a file named harbor.yml in the same directory where run from, harbor.yml structure is:
+ imagetag: <tag to be used on 'docker build'>
+ downloadpath: <local root path to download files into>
+ s3:
+   bucket: <base bucket to download files from>
+   basepath: <inside the bucket the root path for files to be downloaded>
+ files:
+   - s3path: <path to file in S3 after [s3.bucket]/[s3.basepath]>
+     localname: <local path + name of the file, will be downloaded into [downloadpath]/[localname]>
+     permission: <[optional] file permissions, default 0644>
+ - commands:
+   <YAML array containing shell commands (currently /bin/bash) to be run before 'docker build'>
+
+ You can use ${<KEY>} as a placeholder in harbor.yml to be replaced by the value passed in a -e flag
+
+Options:
+  -e []: List of KEY=VALUE to be replaced in harbor.yml
+		`
+		fmt.Println(helpText)
+	}
 }
 
 func checkError(err error) {
