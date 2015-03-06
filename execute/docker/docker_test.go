82b00010d23d7c8b7ee93db3f0aa33959b1e1217
Victor Campos <eu@victorcampos.com.br>
2015-03-05 23:48:41 -0300

    Removes useless test cases for old scenarios

--- execute/docker/docker_test.go
+++ execute/docker/docker_test.go
@@ -5,33 +5,6 @@ import (
 	"time"
 )
 
-func TestBuildTagsShouldAddLatestWhenEmpty(t *testing.T) {
-	emptyTags := make([]string, 0)
-
-	returnedTags := buildTags(emptyTags)
-	if returnedTags[0] != "latest" {
-		t.Error("Expected 'latest' in empty tags slice")
-	}
-}
-
-func TestBuildTagsShouldNotAddLatestIfPresent(t *testing.T) {
-	tagsWithLatest := make([]string, 1)
-	tagsWithLatest[0] = "latest"
-
-	returnedTags := buildTags(tagsWithLatest)
-	latestTagCount := 0
-
-	for _, tag := range returnedTags {
-		if tag == "latest" {
-			latestTagCount++
-		}
-	}
-
-	if latestTagCount != 1 {
-		t.Error("Expected 'latest' once in tags slice, found", latestTagCount)
-	}
-}
-
 func TestCreateTimeBasedVersion(t *testing.T) {
 	myTime := time.Date(2006, time.January, 2, 15, 4, 0, 0, time.UTC)
 
