ea6892c54f2910c92c75fde0893759a942323241
Victor Campos <eu@victorcampos.com.br>
2015-03-07 22:06:03 -0300

    Refactors docker executor code

--- execute/docker/docker.go
+++ execute/docker/docker.go
@@ -8,95 +8,73 @@ import (
 	"time"
 )
 
-func Build(imageTag string, supplementaryTags []string) error {
+func Build(imageName string, tags []string) error {
 	cwd, _ := os.Getwd()
-	buildImageTag := imageTag
 
-	if config.Options.NoLatestTag && len(supplementaryTags) > 0 {
-		// Always create the build image tag from the first supplied tag if -no-latest-tag is set
-		buildImageTag = versionedImageTag(imageTag, supplementaryTags[0])
-	} else if config.Options.NoLatestTag && len(supplementaryTags) == 0 {
-		// Else create a time-based version and avoid creating 'latest' tag
-		version := createTimeBasedVersion(time.Now())
-		buildImageTag = versionedImageTag(imageTag, version)
-		supplementaryTags = append(supplementaryTags, version)
+	if len(tags) == 0 {
+		timeBasedTag := createTimeBasedVersion(time.Now())
+		tags = append([]string{timeBasedTag}, tags...)
 	}
 
-	// FIXME: Use Docker Remote API to perform docker commands
-	// Current usage is too OS-specific and depends on sudo power when executing harbor
-	if err := runDockerCommand("build", "-t", buildImageTag, cwd); err != nil {
+	if !config.Options.NoLatestTag {
+		tags = append([]string{"latest"}, tags...)
+	}
+
+	imageWithTagsList := createImageWithTagsList(imageName, tags)
+
+	if err := runDockerCommand("build", "-t", imageWithTagsList[0], cwd); err != nil {
 		return err
 	}
 
-	finalTags := buildTags(supplementaryTags)
-	for _, tag := range finalTags {
-		if err := createTag(buildImageTag, imageTag, tag); err != nil {
-			return err
-		}
-
-		if !config.Options.NoDockerPush {
-			if err := pushTag(imageTag, tag); err != nil {
+	for i, taggedImage := range imageWithTagsList {
+		if i != 0 {
+			if err := createTag(imageWithTagsList[0], taggedImage); err != nil {
 				return err
 			}
 		}
-	}
 
-	if !config.Options.NoDockerPush && !config.Options.NoLatestTag {
-		if err := pushTag(imageTag, "latest"); err != nil {
-			return err
+		if !config.Options.NoDockerPush {
+			if err := pushTag(taggedImage); err != nil {
+				return err
+			}
 		}
 	}
 
 	return nil
 }
 
-func buildTags(tags []string) []string {
-	finalTags := []string{}
+func createImageWithTagsList(imageName string, tags []string) []string {
+	var imageWithTagsList []string
 
-	if tags != nil {
-		for _, tag := range tags {
-			if tag != "latest" {
-				finalTags = append(finalTags, tag)
-			}
-		}
+	for _, tag := range tags {
+		imageWithTagsList = append(imageWithTagsList, concatenateImageWithTag(imageName, tag))
 	}
 
-	// Adds default version if no additional tag given and -no-latest-tag is unset
-	if len(tags) == 0 && !config.Options.NoLatestTag {
-		// default version format: the current date time in format YYYYMMDDTHHMM
-		version := createTimeBasedVersion(time.Now())
-		finalTags = append(finalTags, version)
-	}
+	return imageWithTagsList
+}
 
-	return finalTags
+func concatenateImageWithTag(imageName string, tag string) string {
+	return imageName + ":" + tag
 }
 
 func createTimeBasedVersion(t time.Time) string {
 	return t.Format("20060102T1504")
 }
 
-func createTag(fromTag string, imageTag string, version string) error {
-	versionedImageTag := versionedImageTag(imageTag, version)
-
-	if err := runDockerCommand("tag", "-f", fromTag, versionedImageTag); err != nil && fromTag != versionedImageTag {
+func createTag(fromTag string, toTag string) error {
+	if err := runDockerCommand("tag", "-f", fromTag, toTag); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func pushTag(imageTag string, version string) error {
-	versionedImageTag := versionedImageTag(imageTag, version)
-
-	if err := runDockerCommand("push", versionedImageTag); err != nil {
+func pushTag(taggedImage string) error {
+	if err := runDockerCommand("push", taggedImage); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func versionedImageTag(imageTag string, version string) string {
-	return fmt.Sprintf("%s:%s", imageTag, version)
 }
 
 func runDockerCommand(parameters ...string) error {
--- execute/docker/docker_test.go
+++ execute/docker/docker_test.go
@@ -14,3 +14,18 @@ func TestCreateTimeBasedVersion(t *testing.T) {
 		t.Error("Expected version 20060102T1504, got", version)
 	}
 }
+
+func TestCreateImageWithTagsList(t *testing.T) {
+	imageName := "test"
+	tags := []string{"latest", "first-tag", "second-tag"}
+
+	imageWithTagsList := createImageWithTagsList(imageName, tags)
+
+	if len(imageWithTagsList) != 3 {
+		t.Fatal("Expected a list of 3 images, got", len(imageWithTagsList))
+	}
+
+	if imageWithTagsList[0] != "test:latest" && imageWithTagsList[1] != "test:first-tag" && imageWithTagsList[2] != "test:second-tag" {
+		t.Fatal("Expected test:latest, test:first-tag and test:second-tag, got", imageWithTagsList)
+	}
+}
